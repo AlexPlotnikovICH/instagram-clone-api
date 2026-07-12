df44a999ee99ca6d788efac050cdfccbfc2a69c2
AlexPlot <plotnikov1988@gmail.com>
2026-07-12 13:16:01 +0200

    fix: resolve bcryptjs import crash and update seeding docs

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import User from './models/userModel.js'
 import Post from './models/postModel.js'
 import Follow from './models/followModel.js'
